34d2812f2d0b981594d05536d976a00536b2fc79
EC2 Default User <ec2-user@ip-172-31-1-156.ec2.internal>
2019-03-04 14:22:33 +0000

    presigned url ok

--- handler.py
+++ handler.py
@@ -1,24 +1,37 @@
 import json
+import os
+import boto3
+import requests
 
+bucketNameParamName = "bucketName"
+bucketName = os.getenv(bucketNameParamName)
+s3 = boto3.client('s3')
 
 def buildSignedUrlUpload(event, context):
+    signedUrlUpload = ""
+    prefix = event['queryStringParameters']['prefix']
+    filename = event['queryStringParameters']['filename']
+    print("prefix: " + prefix)
+    print("filename: " + filename)
+    print("bucketName: " + bucketName)
+    
+    signedUrlUpload = s3.generate_presigned_url(
+        ClientMethod='put_object',
+        Params={
+            'Bucket': bucketName,
+            'Key': prefix + '/' + filename
+        },
+        ExpiresIn=3600
+    )
+    
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event['queryStringParameters']
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": {"signedUrlUpload": signedUrlUpload}
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
