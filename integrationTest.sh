ddf5fcf3550dfd5fa593534ef05ed3590433a7a5
EC2 Default User <ec2-user@ip-172-31-1-156.ec2.internal>
2019-03-08 17:19:34 +0000

    Integration test ready

--- integrationTest.sh
+++ integrationTest.sh
@@ -2,3 +2,5 @@
 
 serviceEndpoint=$(aws cloudformation describe-stacks --stack-name resizePDF-$STAGE --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue' --output text)
 echo $serviceEndpoint
+
+python3 test/integration.py serviceEndpoint
--- test/integration.py
+++ test/integration.py
@@ -8,26 +8,40 @@ import uuid
 
 buildSignedUrlUploadPath = '/buildSignedUrlUpload'
 resizePDFPath = '/resizePDF'
-
 imagesList = ["test/Data/2016-06_Alex_CP_Bulletin1ertrim-page1.jpg","test/Data/2016-06_Alex_CP_Bulletin1ertrim-page2.jpg","test/Data/2016-06_Alex_CP_Bulletin1ertrim-page3.jpg"]
-print (sys.path)
+compression = '50'
+emailAddress = 'benjamin.ehlers@hardis.fr'
+
 def integration(serviceEndpoint):
-    buildSignedUrlUpload_URL = serviceEndpoint + buildSignedUrlUploadPath
-    resizePDF_URL = serviceEndpoint + resizePDFPath
-    
-    for image in imagesList:
+    try:
+        buildSignedUrlUpload_URL = serviceEndpoint + buildSignedUrlUploadPath
+        resizePDF_URL = serviceEndpoint + resizePDFPath
+        
         id = uuid.uuid4()
-        path, filename = os.path.split(image)
-        PARAMS = {'prefix':id, 'filename': filename} 
-        r = requests.get(url = buildSignedUrlUpload_URL, params = PARAMS) 
-  
-        # extracting data in json format 
-        data = r.json()
-        signedUrlUpload = data['signedUrlUpload']
-        print(signedUrlUpload)
-        r = requests.put(signedUrlUpload, data=open(image, 'rb'))
-        print(r.text)
+        
+        for image in imagesList:
+            path, filename = os.path.split(image)
+            PARAMS = {'prefix':id, 'filename': filename} 
+            r = requests.get(url = buildSignedUrlUpload_URL, params = PARAMS) 
+      
+            # extracting data in json format 
+            data = r.json()
+            signedUrlUpload = data['signedUrlUpload']
+            r = requests.put(signedUrlUpload, data=open(image, 'rb'))
+            print("buildSignedUrlUpload_URL: " + str(r.status_code))
+        
+        PARAMS = {'prefix':id, 'compression': compression, 'emailAddress': emailAddress} 
+        r = requests.get(url = resizePDF_URL, params = PARAMS)
+        print("resizePDF: " + str(r.status_code))
+    except Exception as e:
+        print(str(e))
+        raise e
+   
         
 if __name__ == "__main__":
-    serviceEndpoint = 'https://0wqwlvpx17.execute-api.us-east-1.amazonaws.com/dev'
-    integration(serviceEndpoint)
+    try:
+        serviceEndpoint = sys.argv[1] # 'https://kwkkg3ml2j.execute-api.us-east-1.amazonaws.com/dev'
+        integration(serviceEndpoint)
+    except Exception as e:
+        print(str(e))
+        exit(1)
