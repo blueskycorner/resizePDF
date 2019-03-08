64bb5217f27f473439d68b2a4e1cab5f68ee8967
EC2 Default User <ec2-user@ip-172-31-1-156.ec2.internal>
2019-03-08 16:01:13 +0000

    Buildspec.yml fix

--- test/integration.py
+++ test/integration.py
@@ -1,24 +1,33 @@
 import sys
+sys.path.append('.')
 import os
 from buildSignedUrlUpload import buildSignedUrlUpload
-# import resizePDF
+import resizePDF
 import requests
 import uuid
 
-buildSignedUrlUpload_URLParamName = 'buildSignedUrlUpload_URL'
-resizePDF_URLParamName = 'resizePDF_URL'
+buildSignedUrlUploadPath = '/buildSignedUrlUpload'
+resizePDFPath = '/resizePDF'
+
 imagesList = ["test/Data/2016-06_Alex_CP_Bulletin1ertrim-page1.jpg","test/Data/2016-06_Alex_CP_Bulletin1ertrim-page2.jpg","test/Data/2016-06_Alex_CP_Bulletin1ertrim-page3.jpg"]
 print (sys.path)
-def integration():
-    buildSignedUrlUpload_URL = os.getenv(buildSignedUrlUpload_URLParamName)
-    resizePDF_URL = os.getenv(resizePDF_URLParamName)
+def integration(serviceEndpoint):
+    buildSignedUrlUpload_URL = serviceEndpoint + buildSignedUrlUploadPath
+    resizePDF_URL = serviceEndpoint + resizePDFPath
     
-    # for image in imagesList:
-    #     id = uuid.uuid4()
-    #     path, filename = os.path.split(image)
-    #     PARAMS = {'prefix':id, 'filename': filename} 
-    #     r = requests.get(url = buildSignedUrlUpload_URL, params = PARAMS) 
+    for image in imagesList:
+        id = uuid.uuid4()
+        path, filename = os.path.split(image)
+        PARAMS = {'prefix':id, 'filename': filename} 
+        r = requests.get(url = buildSignedUrlUpload_URL, params = PARAMS) 
   
-    #     # extracting data in json format 
-    #     data = r.json() 
-    #     print(data)
+        # extracting data in json format 
+        data = r.json()
+        signedUrlUpload = data['signedUrlUpload']
+        print(signedUrlUpload)
+        r = requests.put(signedUrlUpload, data=open(image, 'rb'))
+        print(r.text)
+        
+if __name__ == "__main__":
+    serviceEndpoint = 'https://0wqwlvpx17.execute-api.us-east-1.amazonaws.com/dev'
+    integration(serviceEndpoint)
